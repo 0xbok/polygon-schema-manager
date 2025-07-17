61b6a229524f3c3d1a29482fd02938a00ce3ee19
0xbok <1689531+0xbok@users.noreply.github.com>
2025-07-17 00:00:00 +0000

    verify _id signature to create schema

--- contracts/SchemaRegistry.sol
+++ contracts/SchemaRegistry.sol
@@ -6,94 +6,38 @@ pragma solidity 0.8.16;
  * @dev Smart Contract for Polygon Schema Methods
  */
 contract SchemaRegistry {
-    address private owner;
+    mapping(address _id => mapping(string schemaId => string schema)) public schemas;
+    mapping(address _id => uint nextNonce) public nonce;
 
-    struct Schema {
-        string schemaJson;
-    }
-
-    mapping(address => mapping(string => Schema)) private schemas;
-
-    event TransferOwnership(address indexed newOwner);
     event SchemaCreate(address indexed id, string schemaId, string schemaJson);
-    bool private initialized;
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, 'Message sender is not the owner');
-        _;
-    }
-
-    modifier nonReentrant() {
-        require(!initialized, 'Contract instance has already been initialized');
-        initialized = true;
-        _;
-    }
-
-    /**
-     * @dev Initializes the ownership of the contract.
-     */
-    function initialize() external nonReentrant {
-        require(msg.sender != address(0), 'Invalid owner address');
-        owner = msg.sender;
-    }
-
-    /**
-     * @dev Transfers the ownership of the contract.
-     * @param _newOwner Address of the new owner.
-     * @return Message indicating the success or failure of ownership transfer.
-     */
-    function transferOwnership(
-        address _newOwner
-    ) external onlyOwner returns (string memory) {
-        require(_newOwner != address(0), 'Invalid owner address');
-        require(
-            owner != _newOwner,
-            'Ownership cannot be transferred to the same account'
-        );
-        owner = _newOwner;
-        emit TransferOwnership(owner);
-        return ('Ownership transferred successfully');
-    }
 
     /*
-     * @dev Reads the contract owner from the chain.
-     * @return The address of the contract owner.
-     */
-    function getOwner() public view returns (address _owner) {
-        return owner;
-    }
-
-    /*
-     * @dev Creates a new schema.
+     * @dev Creates a new schema with signature verification.
      * @param _id Address identifier.
      * @param newSchemaId Identifier for the new schema.
      * @param _json JSON representation of the schema.
-     * @return The created schema ID.
+     * @param r Signature's r.
+     * @param s Signature's s.
+     * @param v Signature's v.
      */
     function createSchema(
         address _id,
         string memory newSchemaId,
-        string memory _json
-    ) external returns (string memory schemaId) {
+        string memory _json,
+        bytes32 r,
+        bytes32 s,
+        uint8 v
+    ) external {
         require(_id != address(0), 'Invalid address provided');
-        Schema memory newSchema = Schema(_json);
-        schemas[_id][newSchemaId] = newSchema;
 
-        emit SchemaCreate(msg.sender, newSchemaId, _json);
-        return newSchemaId;
-    }
+        bytes32 messageHash = keccak256(abi.encodePacked(_id, newSchemaId, _json, nonce[_id]++));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
 
-    /**
-     * @dev Gets a schema by its ID.
-     * @param _id Address identifier.
-     * @param _schemaId Identifier of the schema.
-     * @return The schema object.
-     */
-    function getSchemaById(
-        address _id,
-        string memory _schemaId
-    ) external view returns (Schema memory) {
-        Schema memory schema = schemas[_id][_schemaId];
-        return schema;
+        address signer = ecrecover(ethSignedMessageHash, v, r, s);
+        require(signer == _id, 'Invalid signature');
+
+        schemas[_id][newSchemaId] = _json;
+
+        emit SchemaCreate(_id, newSchemaId, _json);
     }
 }
