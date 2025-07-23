84fefebc97b5ab9c206d20eacd223f4a4dd9dfb9
0xbok <1689531+0xbok@users.noreply.github.com>
2025-07-23 00:00:00 +0000

    only _id can call createSchema

--- contracts/SchemaRegistry.sol
+++ contracts/SchemaRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.16;
+pragma solidity ^0.8.28;
 
 /**
  * @title SchemaRegistry
@@ -7,37 +7,19 @@ pragma solidity 0.8.16;
  */
 contract SchemaRegistry {
     mapping(address _id => mapping(string schemaId => string schema)) public schemas;
-    mapping(address _id => uint nextNonce) public nonce;
 
     event SchemaCreate(address indexed id, string schemaId, string schemaJson);
 
     /*
-     * @dev Creates a new schema with signature verification.
-     * @param _id Address identifier.
+     * @dev Creates a new schema.
      * @param newSchemaId Identifier for the new schema.
      * @param _json JSON representation of the schema.
-     * @param r Signature's r.
-     * @param s Signature's s.
-     * @param v Signature's v.
      */
     function createSchema(
-        address _id,
         string memory newSchemaId,
-        string memory _json,
-        bytes32 r,
-        bytes32 s,
-        uint8 v
+        string memory _json
     ) external {
-        require(_id != address(0), 'Invalid address provided');
-
-        bytes32 messageHash = keccak256(abi.encodePacked(_id, newSchemaId, _json, nonce[_id]++));
-        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
-
-        address signer = ecrecover(ethSignedMessageHash, v, r, s);
-        require(signer == _id, 'Invalid signature');
-
-        schemas[_id][newSchemaId] = _json;
-
-        emit SchemaCreate(_id, newSchemaId, _json);
+        schemas[msg.sender][newSchemaId] = _json;
+        emit SchemaCreate(msg.sender, newSchemaId, _json);
     }
 }
